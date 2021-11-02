dd22b174cc7825371a5e5550d0cce764beaa17d2
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-11-02 15:56:36 +0530

    Add files via upload

--- Ex2.ts
+++ Ex2.ts
@@ -4,6 +4,7 @@ class Car {
    
    //constructor 
    constructor(engine:string) { 
+      console.log("cons called");
       this.engine = engine 
    }  
    
@@ -15,9 +16,11 @@ class Car {
 
 //create an object 
 var obj = new Car("Maruti")
-
+var obj1 = new Car("Honda")
+var obj2 = new Car("Etios")
 //access the field 
 console.log("Reading attribute value Engine as :  "+obj.engine)  
-
+console.log("Reading attribute value Engine as :  "+obj1.engine)  
+console.log("Reading attribute value Engine as :  "+obj2.engine)  
 //access the function
 obj.disp()
